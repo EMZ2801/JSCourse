548f0bcda3e595158fb91ed3ec2246a262520fd9
EMZ2801 <158743772+EMZ2801@users.noreply.github.com>
2024-02-19 14:14:37 +0300

    Update HW_4.js

--- HW_4.js
+++ HW_4.js
@@ -1,85 +1,79 @@
-//1. Напишите функцию, которая принимает в себя строку в виде аргумента и возвращает true, если функции палиндром. Палиндром - строка, которая читается с конца так же, как и с начала. Условие - должны учитываться только буквы без знаков препинания. 
+//1. Напишите функцию, которая принимает в себя строку в виде аргумента и возвращает true, если функции палиндром. Палиндром - строка, которая читается с конца так же, как и с начала. Условие - должны учитываться только буквы без знаков препинания.
 
 /* f("tenet") // true */
 /* f("tenet!") // true */
 
+function chekIsPalinmrom(str) {
+  str = str.toLowerCase().replaceAll(/[.,!?"';:()-\s]/g, "");
+  return str === str.split("").reverse().join("");
+}
 
-function isPalinmrom(str) {
-    str = str.toLowerCase().
-    replaceAll (/[.,!?"';:()-\s]/g, '');
-    return str ===str.split('').reverse().join('');
- }
- 
- console.log(isPalinmrom('Заказ  ! шалаш? заказ.')) 
-
+console.log(chekIsPalinmrom("Закaз  ! шалаш? заказ."));
 
 //2. Напишите функцию, принимающую строку и возвращающую самое длинное слово в строке
 // f('Web Development Tutorial'); // "Development"
 
-
-let str2='';
-let longestWords = 0;
-let iMax=0;
-function islongesttWords(str1) {
-   str2 = str1.split(' ')
-   for (let i=0; i<str2.length; i++) {
-      if (str2[i].length > longestWords) {
-         longestWords = str2[i].length;
-         iMax = i;
-      }
-   }
+let extraStr = "";
+let lengthOflongestWord = 0;
+let indexOfLongestWord = 0;
+function getlongesttWords(str) {
+  extraStr = str.split(" ");
+  for (let i = 0; i < extraStr.length; i++) {
+    if (extraStr[i].length > lengthOflongestWord) {
+      lengthOflongestWord = extraStr[i].length;
+      indexOfLongestWord = i;
+    }
+  }
 }
 
-islongesttWords('Web Development Tutorial ');
-console.log (str2[iMax])
-
+getlongesttWords("Web Development Tutorial");
+console.log(extraStr[indexOfLongestWord]);
 
 //3. Напишите функцию, принимающую аргумент в виде строки и которая расставляет все буквы строки в алфавитном порядке. Возвращает новую строку с буквами в алфавитном порядке
-/* f("alphabetical");  */// "aaabcehillpt"
+/* f("alphabetical");  */ // "aaabcehillpt"
 
-let str2
-function isAlphabetView(str) {
-   str = str.toLowerCase().trim().split('').sort();
-   str2 = str.join('');
+let newalphabetStr;
+function getAlphabetView(str) {
+  str = str.toLowerCase().trim().split("").sort();
+  newalphabetStr = str.join("");
 }
-isAlphabetView('aLphabetical')
-console.log (str2)
+getAlphabetView("aLphabetical");
+console.log(newalphabetStr);
 
 /* 4. Напишите функцию JavaScript, которая принимает строку и возвращает индекс первого неповторяющегося символа. 
 f('фывфавыапрс'); // 8 */
 
 function isNotRepeated(str) {
-    for (let i = 0; i < str.length; i++) {
-      let control = 1;
-      for (let i2 = 0; i2 < str.length; i2++) {
-        if (str[i] === str[i2] && i !== i2) {
-          control = 0;
-        }
+  for (let i = 0; i < str.length; i++) {
+    let control = 1;
+    for (let j = 0; j < str.length; j++) {
+      if (str[i] === str[j] && i !== j) {
+        control = 0;
       }
-      if (control) return i;
     }
-    return "нет уникального значения";
+    if (control) return i;
   }
-  
-  console.log(isNotRepeated("ppdfd"));
+  return "нет уникального значения";
+}
+
+console.log(isNotRepeated("ppdfd"));
 
 /* 5. Напишите функцию, которая принимает строку в качестве параметра и возвращает количество гласных в строке.
 f('Web Development Tutorial')); //9
  */
 
-let calc = 0;
-let str2='aeiouy';
+let quantityVowels = 0;
+let vowelsStr = "aeiouy";
 function getVowels(str) {
-   str=str.toLowerCase().split('')
-   str2=str2.split('');
-   console.log(str,str2)
-   for (let i=0; i<str.length; i++) {
-      for (let i2=0; i2<str2.length; i2++) {
-         if (str[i]===str2[i2]) {
-            calc = calc +1;
-         }
+  str = str.toLowerCase().split("");
+  vowelsStr = vowelsStr.split("");
+  for (let i = 0; i < str.length; i++) {
+    for (let y = 0; y < vowelsStr.length; y++) {
+      if (str[i] === vowelsStr[y]) {
+        quantityVowels = quantityVowels + 1;
       }
-   }
-   return calc;
+    }
+  }
+  return quantityVowels;
 }
-console.log(getVowels('WEb Development Tutorial'))
+console.log(getVowels("WEb Development Tutorial"));
