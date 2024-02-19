19333278d36ad2a24c6a80bf2b84ee7990f10a8f
EMZ2801 <158743772+EMZ2801@users.noreply.github.com>
2024-02-20 01:22:08 +0300

    Update CW4.js

--- CW4.js
+++ CW4.js
@@ -1,4 +1,67 @@
-/*
+/*1
+const person = {
+  name: "Pete",
+  age: 24,
+  salary: 2550,
+  street: "Some street",
+  houseNum: 52
+}
+
+Создайте функцию, которая выводит среднее арифметическое всех числовых значений объекта и выводит результат в консоль
+ */
+
+function getAverageSum(argument) {
+  let isSum = 0;
+  let i = 0;
+  for (let key in person) {
+    if (parseFloat(person[key])) {
+      isSum += parseFloat(person[key]);
+      i++;
+    }
+  }
+  if (i > 0) {
+    return +(isSum / i).toFixed(2);
+  } else {
+    return "числовые значения объекта отсутствуют";
+  }
+}
+const argument = (person = {
+  name: "Pete",
+  age: 24,
+  salary: 2550,
+  street: "Some street",
+  houseNum: 52,
+});
+
+console.log(getAverageSum(argument));
+
+/*2
+Напишите кастомный Object.assign,
+который принимает на вход целевой объект для копирования и копируемый объект и возвращает целевой
+    function customAssign(target, source) {
+        //code
+        return target
+    }
+ */
+
+    function customAssign(target, source) {
+      target = Object.assign(target, source);
+      return target;
+    }
+    const target = {
+      name: "Kate",
+      age: 30,
+    };
+    
+    const source = {
+      firstname: "Ivanov",
+      name: "Polina",
+      id: "248",
+    };
+    console.log(customAssign(target, source))
+
+
+/*3
 //TODO: максимально часто встречающаяся задача на фронте
 
 Необходимо создать функцию formatDate,
@@ -18,13 +81,21 @@ const formattedDate = formatDate(currentDate);
 Подсказка: Месяцы начинаются с нуля
 */
 
-function formatDate(date) {
-  const newFormat = `${date.getDate()}.${date.getMonth() + 1 < 10 ? "0" : ""}${
-    date.getMonth() + 1}.${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-
-  console.log(newFormat); // Выведет строку в формате "DD.MM.YYYY, HH:mm:ss"
-  return newFormat;
+function getNewformatDate(date) {
+  const isNewFormatDate = `${
+    date.getDate() + 1 < 10 ? "0" : ""
+  }${date.getDate()}.${date.getMonth() + 1 < 10 ? "0" : ""}${
+    date.getMonth() + 1
+  }.${date.getFullYear()}, ${
+    date.getHours() + 1 < 10 ? "0" : ""
+  }${date.getHours()}:${
+    date.getMinutes() + 1 < 10 ? "0" : ""
+  }${date.getMinutes()}:${
+    date.getSeconds() + 1 < 10 ? "0" : ""
+  }${date.getSeconds()}`;
+  console.log(isNewFormatDate); // Выведет строку в формате "DD.MM.YYYY, HH:mm:ss"
+  return isNewFormatDate;
 }
 
 const date = new Date();
-console.log(formatDate(date));
+getNewformatDate(date)
