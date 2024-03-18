824b7102a0d34866ee1e8c2a974f596e8069ac00
EMZ2801 <Ekaterina.dubei@mail.ru>
2024-03-18 12:36:11 +0300

    commit site

---
+++ HTML+CSS/HW/script.js
@@ -0,0 +1,20 @@
+const carouselWrapper = document.querySelector('.carousel__wrapper')
+
+console.log('carouselWrapper', carouselWrapper)
+
+carouselWrapper.addEventListener('click', (event) => {
+    const rightElement =  getComputedStyle(event.currentTarget).right.replace('px', '')
+
+    const isLeftClick = (20 + 700 + 8) > event.pageX
+    if (isLeftClick) {
+        if (rightElement <= 0) return
+
+        event.currentTarget.style.right = `${+rightElement - 480}px`
+    } else {
+        if (rightElement >= 900) return
+
+        event.currentTarget.style.right = `${+rightElement + 480}px`
+    }
+})
+
+
